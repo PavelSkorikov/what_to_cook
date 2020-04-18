e010ac2491f8661003a625562b8a900ed4234261
PavelSkorikov <spg75@yandex.ru>
2020-04-18 11:50:45 +0300

    add models Comments and refactoring models and admins

--- main_app/admin.py
+++ main_app/admin.py
@@ -4,8 +4,23 @@ from main_app.models import *
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
     """форма рецепта"""
-    list_display = ('title', 'description', 'image', 'steps', 'createAt', 'user')
+    list_display = ('title',
+                    'description',
+                    'image',
+                    'dish',
+                    'ingredients',
+                    'steps',
+                    'procedure',
+                    'createAt',
+                    'user',
+                    'cooking_time',
+                    'servings',
+                    'likes',
+                    'dislikes',
+                    'complexity',
+                    )
     list_display_links = ('title',)
+    list_filter = ('title', 'user', 'createAt', 'cooking_time', 'dish', 'likes', 'dislikes', 'complexity',)
 
 @admin.register(Ingridient)
 class IngridientAdmin(admin.ModelAdmin):
@@ -18,3 +33,10 @@ class StepsAdmin(admin.ModelAdmin):
     """форма этапа приготовления"""
     list_display = ('number', 'image', 'action')
     list_display_links = ('number',)
+
+@admin.register(Comments)
+class CommentsAdmin(admin.ModelAdmin):
+    """коментарий"""
+    list_display = ('comment', 'user', 'recipe', 'createAt')
+    list_display_links = ('comment', 'recipe', 'user')
+    list_filter = ('user', 'recipe', 'createAt')
--- main_app/models.py
+++ main_app/models.py
@@ -14,6 +14,8 @@ class Ingridient(models.Model):
     class Meta:
         verbose_name = "Ингредиент"
         verbose_name_plural = "Ингредиенты"
+    def __str__(self):
+        return self.name
 
 
 class Steps(models.Model):
@@ -27,6 +29,9 @@ class Steps(models.Model):
     class Meta:
         verbose_name = "Этап приготовления"
         verbose_name_plural = "Этапы приготовления"
+    def __str__(self):
+        num = str(self.number)
+        return num
 
 
 class Recipe(models.Model):
@@ -42,19 +47,49 @@ class Recipe(models.Model):
         ('DRINK', 'НАПИТКИ')
     )
     dish = models.CharField("Блюдо", max_length=10, choices=DISH_NAME, default='NO' )
-    description = models.TextField("Описание", max_length=5000, unique=True)
+    description = models.TextField("Описание", max_length=5000, blank=True)
+    ingredients = models.TextField("Ингредиенты", max_length=5000, default=None, null=True)
     procedure = models.TextField("Порядок приготовления", max_length=5000, default=None)
     # file will be saved to MEDIA_ROOT/uploads/2015/01/30
     image = models.ImageField("Изображение", upload_to='uploads/%Y/%m/%d/', height_field=None, width_field=None,
                               max_length=100)
+    cooking_time = models.CharField("Время приготовления", max_length=50, default='0 мин', blank=True)
+    servings = models.IntegerField("Количество порций", default=1, blank=True)
+    likes = models.IntegerField("Лайки", default=0, blank=True)
+    dislikes = models.IntegerField("Дизлайки", default=0, blank=True)
+    COMPLEXITY = (
+        ('EASY', 'Простой'),
+        ('MIDLE', 'Средний'),
+        ('HARD', 'Сложный'),
+        ('SUPER_HARD', 'Очень сложный'),
+    )
+    complexity = models.CharField("Сложность приготовления", max_length=50, choices=COMPLEXITY, default='Простой', blank=True)
     steps = models.ForeignKey(Steps, verbose_name='этапы пригтовления', null=True, blank=True, on_delete=models.SET_NULL)
     # дата создания записи в базе
     createAt = models.DateField("Дата создания", auto_now_add=True)
     # связанная модель пользователя, который оставил рецепт
     user = models.ForeignKey(User, verbose_name='автор', on_delete=models.SET('remoted_user'))
 
-    # сортируем по дате создания записи
     class Meta:
         ordering = ['createAt']
         verbose_name = "Рецепт"
         verbose_name_plural = "Рецепты"
+
+    def __str__(self):
+        return self.title
+
+class Comments(models.Model):
+    # комментарии к рецепту
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    comment = models.TextField("Комментарий к рецепту", max_length=5000, default=None)
+    user = models.ForeignKey(User, verbose_name='автор комментария', on_delete=models.SET('anonimous_user'))
+    recipe = models.ForeignKey(Recipe, verbose_name='рецепт', on_delete=models.CASCADE)
+    createAt = models.DateField("Дата создания", auto_now_add=True)
+
+    class Meta:
+        ordering = ['createAt']
+        verbose_name = "Комментарий"
+        verbose_name_plural = "Комменетарии"
+
+    def __str__(self):
+        return self.recipe
